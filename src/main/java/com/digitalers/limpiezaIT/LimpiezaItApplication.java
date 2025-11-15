53a227a5e8ff3936ec7ae046ecd834b25428ab65
Jorge <jorge.lewin15@gmail.com>
2025-11-15 16:45:16 -0300

    Subo la Estructura del Proyecto

---
+++ src/main/java/com/digitalers/limpiezaIT/LimpiezaItApplication.java
@@ -0,0 +1,13 @@
+package com.digitalers.limpiezaIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimpiezaItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimpiezaItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalers/limpiezaIT/LimpiezaItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalers.limpiezaIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimpiezaItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
